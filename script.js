252ecf7589a14f672c1dd73c92a3da4dc04bbebd
Sahilsingh1998 <99865976+Sahilsingh1998@users.noreply.github.com>
2023-07-17 15:33:45 +0530

    updated color

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
+const colors = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
 
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
+
+var setBg = () => {
+    var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    document.body.style.backgroundColor = "#" + randomColor;
+    return "#" + randomColor;
+}
+
 btn.addEventListener("click", function () {
-    const randomNumber = getrandomNumber();
-    console.log(randomNumber);
-
-    document.body.style.backgroundColor = colors[randomNumber];
-
-    color.textContent = colors[randomNumber];
-
+    let hexColor = "#";
+    document.body.style.backgroundColor = setBg();
 });
 
-function getrandomNumber() {
-    return Math.floor(Math.random() * colors.length);
-
+function getRandomNumber() {
+    return Math.floor(Math.random() * hex.length);
 }
